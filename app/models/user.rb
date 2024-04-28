b1a86e14b5324f895d8d3e88ba8ef3f59fd721bc
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-28 03:55:26 +0000

    Add uniqueness validation for fan_id and photo_id in Like model; created sample data

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,13 +28,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: "author_id"
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many "followers", through: :accepted_sent_follow_requests, source: :sender
+  has_many :followers, through: :accepted_sent_follow_requests, source: :sender
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :likes, foreign_key: :fan_id
   has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
